f804996adee3b11b3fb4f5cbe45b30653d6ad249
Thiago Prates <tsprates@hotmail.com>
2020-10-01 21:48:01 -0300

    user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,14 +15,14 @@ class UserSeeder extends Seeder
     {
         $user = new User;
         $user->name = 'admin';
-        $user->account = '1234';
+        $user->account = '123';
         $user->email = 'admin@test.com';
         $user->password = Hash::make('secret');
         $user->save();
         
         $user = new User;
         $user->name = 'User 1';
-        $user->account = '123';
+        $user->account = '1234';
         $user->email = 'user1@test.com';
         $user->password = Hash::make('secret');
         $user->save();
